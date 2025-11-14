b1ae0c20fe2427f47f0ffa856d86918e06a7eca1
Samuel-Bautista <u20241220454@usco.edu.co>
2025-11-14 16:26:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/Somos.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "somos.html"));
+});
+
 app.use("/login", loginRouter);
 app.use('/dashboard', dashboardRoutes);
 app.use('/adopcion', adopcionRoutes);
@@ -28,4 +32,5 @@ app.use('/voluntariado', voluntariadoRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
+
 
